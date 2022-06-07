cad5837c97ce92299d8ce3f6103cc8444fb805f8
Nicolas Jara <nicolas.jara.1994@hotmail.com>
2022-06-07 15:43:16 -0400

    1

---
+++ hooks/useFetch.js
@@ -0,0 +1,53 @@
+import { useEffect, useState, useRef } from "react"
+
+
+export const useFetch = (url) => {
+
+    const isMounted = useRef(true)
+    
+    const [state, setState] = useState({data: null, loading: true, error:null});
+
+    useEffect(() => {
+      
+    
+      return () => {
+        isMounted.current = false
+      }
+    }, [])
+    
+
+    useEffect(() => {
+
+      setState({data:null, loading:true, error:null})
+
+      fetch(url)
+      .then( resp => resp.json())
+      .then( data => {
+
+
+        setTimeout(() => {
+
+          if(isMounted.current){
+            setState({
+              loading: false,
+              error: null,
+              data
+          });
+          }else{
+            console.log('setState no se llamó');
+          }
+
+          
+        }, 1000);
+
+            
+        
+      });
+    
+      
+    }, [url]);
+
+    return state;
+    
+
+}
